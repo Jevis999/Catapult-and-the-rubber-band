d77ffcd9a9ffce02fd8a61603ea29b719397767a
unknown <you@example.com>
2020-04-26 11:07:19 +0530

    created a catapult

--- sketch.js
+++ sketch.js
@@ -36,10 +36,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,60);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingShot = new SlingShot(bird.body,{x:200, y:60});
 }
 
 function draw(){
@@ -64,7 +64,7 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingShot.display();    
 }
 
 function mouseDragged(){
@@ -73,5 +73,5 @@ function mouseDragged(){
 
 
 function mouseReleased(){
-    slingshot.fly();
+    slingShot.fly();
 }
